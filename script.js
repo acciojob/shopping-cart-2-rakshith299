1d82f2db60357cec2bb4b550d87825ade00c733a
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-17 21:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your code here
+function addToTable(){
+    const name = document.getElementById("item-name-input");
+    const price = document.getElementById("item-price-input");
+    let total = document.getElementById("total");
+
+    let nameVal = name.value;
+    let priceVal = price.value;
+    let curPrice = Number(total.innerText);
+
+
+    curPrice = curPrice + Number(priceVal);
+    total.innerText = curPrice;
+
+    let newrow = document.createElement("tr");
+    newrow.classList.add("row");
+
+    let nameCol = document.createElement("td");
+    nameCol.classList.add("col");
+    nameCol.innerText = nameVal;
+
+    let priceCol = document.createElement("td");   
+    nameCol.classList.add("col");
+    priceCol.innerText = priceVal;
+
+    newrow.appendChild(nameCol);
+    newrow.appendChild(priceCol);
+
+    let table = document.getElementById("table");
+    table.appendChild(newrow);
+
+
+
+}
